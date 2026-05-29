bee8a90cf1db27a090c70c0e8a40d05d6e010f35
lytrungnam <namly1.15052004@gmail.com>
2026-05-29 21:05:33 +0700

    fix: use shell form CMD to resolve PORT env var in Railway

--- ngrok_start.py
+++
@@ -1,6 +0,0 @@
-from pyngrok import conf, ngrok
-
-conf.get_default().auth_token = "3EOcPPut4qHD2ZfVzoolUrz6gfu_69ZhLjtgNV6rmd1CXtLCX"
-t = ngrok.connect(8000, domain="rotunda-gibberish-brute.ngrok-free.dev")
-print("URL:", t.public_url)
-input("Nhan Enter de dung ngrok...")
